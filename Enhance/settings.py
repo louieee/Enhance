206de62379c31a46323bf6c929a8a9bd351cdc11
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-08-26 15:19:17 +0100

    static

--- Enhance/settings.py
+++ Enhance/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = ['enhanceit.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Account',
-    'Account.apps.EnhanceAdminConfig'
+    'Account'
 ]
 
 MIDDLEWARE = [
